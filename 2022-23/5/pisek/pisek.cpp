812d4f28102e6288efe7d9a780135550abb4d209
ZDVokoun <grebe_hygrology@aleeas.com>
2023-05-08 20:40:18 +0200

    Done task Pisek

--- 2022-23/5/pisek/pisek.cpp
+++ 2022-23/5/pisek/pisek.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <bits/stdc++.h>
 
 #define ll long long
@@ -7,84 +8,44 @@ using namespace std;
 int main(int argc, char *argv[]) {
   int N;
   cin >> N;
-  vector<int> town(N);
+  vector<ll> town(N);
   for (int i = 0; i < N; i++)
     cin >> town[i];
 
+  vector<ll> firstWay(N);
+  ll h = town[0];
+  for (int i = 0; i < N; i++) {
+    if (h < town[i])
+      h = town[i];
+    h++;
+    firstWay[i] = h;
+  }
+  vector<ll> secondWay(N);
+  h = town[N - 1];
+  for (int i = N - 1; i >= 0; i--) {
+    if (h < town[i])
+      h = town[i];
+    h++;
+    secondWay[i] = h;
+  }
+  vector<ll> finalWay(N);
   ll sand = 0;
-  int s = 0, e = N - 1;
-  bool done = false;
-  bool wasOdd = false;
-  while (!done) {
-    cout << s << " " << e << endl;
-    int h = 0;
-    int bound = s + int((e - s) / 2);
-    bool odd = (e - s + 1) % 2;
-    if (town[s] > town[e]) {
-      bound = s + int((e - s) / 2);
-      // if ((e - s) % 2 == 1) bound += 1;
-      for (int i = e; i > bound - 1; i--) {
-        e = i;
-        if (i == bound) {
-          done = true;
-          break;
-        }
-        if (h <= town[i])
-          break;
-        h += 1;
-        sand += h - town[i];
-        // town[i] = h;
-      }
-      if (done) {
-        if (odd)
-          h++;
-        for (int i = e; i > s - 1; i--) {
-          if (h <= town[i]) {
-            sand++;
-            h--;
-            continue;
-          }
-          sand += h - town[i];
-          h--;
-        }
-        wasOdd = odd;
-      }
-    } else {
-      h = town[s];
-      bound = s + int((e - s) / 2);
-      if (!odd)
-        bound++;
-      for (int i = s; i < bound + 1; i++) {
-        s = i;
-        if (i == bound) {
-          done = true;
-          break;
-        }
-        if (h <= town[i])
-          break;
-        h += 1;
-        sand += h - town[i];
-        // town[i] = h;
-      }
-      // cout << sand << endl;
-      if (done) {
-        if (odd)
-          h++;
-        for (int i = s; i < e + 1; i++) {
-          if (h <= town[i]) {
-            sand++;
-            h--;
-            continue;
-          }
-          sand += h - town[i];
-          h--;
-        }
-        wasOdd = odd;
-      }
-    }
+  for (int i = 0; i < N; i++) {
+    finalWay[i] = min(firstWay[i], secondWay[i]);
+    sand += finalWay[i] - town[i];
+  }
+
+  ll largest = 0;
+  int smallestOccurence = 0;
+  for (int i = 0; i < N; i++) {
+    if (largest < finalWay[i]) {
+      largest = finalWay[i];
+      smallestOccurence = 1;
+    } else if (largest == finalWay[i])
+      smallestOccurence++;
   }
   ll res = 0;
-  if (wasOdd)
+  if (smallestOccurence % 2 == 1)
     res += N * 2 - 1;
   else
     res += N * 2;
